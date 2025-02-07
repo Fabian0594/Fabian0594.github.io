c0479b30cc31fcacc64610137fe51bcd3baecae4
Fabian0594 <142474336+Fabian0594@users.noreply.github.com>
2025-02-07 17:50:43 +0000

    Saludo

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const text = "hi, how is everything going world? ";
+    const typingElement = document.querySelector(".typing");
+    typingElement.textContent = text;
+});
 
