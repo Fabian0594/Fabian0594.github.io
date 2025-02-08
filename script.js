12195b69324c6afcff067a364b9d7612ecce6990
Fabian0594 <142474336+Fabian0594@users.noreply.github.com>
2025-02-08 23:26:19 +0000

    OptionBar Added

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+document.getElementById('menuButton').addEventListener('click', function(e) {
+    e.preventDefault();
+    var submenu = document.getElementById('submenu');
+    submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
+});
